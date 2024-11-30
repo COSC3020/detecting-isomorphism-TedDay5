8889ae5c5090b318c9a8a20803296a081df314d5
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-11-30 07:38:23 -0700

    defining functions

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
-are_isomorphic(graph1, graph2) {
-    return undefined;
+function are_isomorphic(graph1, graph2) {
+    if (graph1.length !== graph2.length) {
+        return false;
+    }
+    
+}
+
+function is_valid(source, target, size, vertex) {
+
+}
+
+function build_node_connections(adjList) {
+
+}
+
+function mapping(source, target, size, vertex, current, visited) {
+
 }
