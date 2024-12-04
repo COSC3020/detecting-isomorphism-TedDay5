7afe53cada8e81000d79dac79476df3abd5108d5
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-03 22:06:23 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const assert = require('assert');
+const jsc = require('jsverify');
 
 eval(fs.readFileSync('code.js')+'');
 
@@ -24,6 +24,18 @@ const graphC = [
     [0, 3]
 ]
 
-assert(are_isomorphic(graphA, graphB) === true);
-assert(are_isomorphic(graphA, graphC) === false);
-assert(are_isomorphic(graphB, graphC) === false);
+const tests = [
+    {function: are_isomorphic, graph1: graphA, graph2: graphB, result: true, name: 'Isomorphic Test' },
+    {function: are_isomorphic, graph1: graphA, graph2: graphC, result: true, name: 'Non-Isomorphic Test' },
+    {function: are_isomorphic, graph1: graphB, graph2: graphC, result: true, name: 'Non-Isomorphic Test' },
+];
+
+tests.forEach(test => {
+    const output = test.function(test.graph1, test.graph2);
+    if (output === test.result) {
+        console.log('${test.name} PASSED!');
+    }
+    else {
+        console.error('${test.name} FAILED.: ${output} != ${test.result}');
+    }
+});
