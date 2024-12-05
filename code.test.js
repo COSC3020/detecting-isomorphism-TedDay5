07b0a2a51d7c6da154c4b1a99cd252107faf21df
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-04 21:14:36 -0700

    Expanded code.test.js

--- code.test.js
+++ code.test.js
@@ -17,12 +17,82 @@ const graphB = [
     [0, 1, 1, 0]
 ]
 
-const graphC = [
+const graphSmallC = [
     [1, 0, 1],
     [0, 1, 0],
+    [1, 0, 1]
+]
+
+const graphSmallD = [
     [1, 0, 1],
+    [0, 1, 0],
+    [1, 0, 1]
+]
+
+const graphFullE = [
+    [0, 1, 1, 1],
+    [1, 0, 1, 1],
+    [1, 1, 0, 1],
+    [1, 1, 1, 0]
+]
+
+const graphFullF = [
+    [0, 1, 1, 1],
+    [1, 0, 1, 1],
+    [1, 1, 0, 1],
+    [1, 1, 1, 0]
+]
+
+const graphDifferentG = [
+    [0, 1, 0, 1],
+    [1, 0, 0, 0],
+    [1, 0, 0, 1],
+    [0, 1, 1, 0]
+]
+
+const graphDifferentH = [
+    [0, 1, 0, 1],
+    [1, 0, 0, 0],
+    [1, 0, 0, 1],
+    [0, 1, 1, 0]
+]
+
+const graphLargeI = [
+    [0, 1, 1, 1, 0],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [0, 1, 1, 1, 0]
+]
+
+const graphLargeJ = [
+    [0, 1, 1, 1, 0],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [0, 1, 1, 1, 0]
+]
+
+const emptyK = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+const emptyL = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
 ]
 
 assert(are_isomorphic(graphA, graphB) === true);
-assert(are_isomorphic(graphA, graphC) === false);
-assert(are_isomorphic(graphB, graphC) === false);
+assert(are_isomorphic(graphA, graphSmallC) === false);
+assert(are_isomorphic(graphB, graphSmallC) === false);
+assert(are_isomorphic(graphSmallC, graphSmallD) === true);
+assert(are_isomorphic(graphFullE, graphFullF) === true);
+assert(are_isomorphic(graphA, graphFullF) === false);
+assert(are_isomorphic(graphDifferentG, graphDifferentH) === true);
+assert(are_isomorphic(graphA, graphDifferentH) === false);
+assert(are_isomorphic(graphLargeI, graphLargeJ) === true);
+assert(are_isomorphic(emptyK, emptyL) === true);
+assert(are_isomorphic(graphA, emptyL) === false);
